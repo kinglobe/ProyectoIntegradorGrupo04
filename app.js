1e29bf53809f76330d8c5588f695fcc45f8a81f9
Okami <omarzerpa316@gmail.com>
2023-07-12 22:48:53 -0300

    modificacion de estructuta

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ app.use(express.static('public'));
 /* rutas */
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')));
 
+app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')));
+
+app.get('/registro',(req,res) => res.sendFile(path.join(__dirname,'views','registro.html')));
+
+app.get('/detalle-producto',(req,res) => res.sendFile(path.join(__dirname,'views','detalle-producto.html')));
+
 
 
 app.listen(PORT, () => console.log(`server running in http://localhost:${PORT}`));
