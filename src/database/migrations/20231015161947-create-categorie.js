90266eb372eac198bb05076538613be9ba74d709
Okami <omarzerpa316@gmail.com>
2023-10-16 17:37:58 -0300

    creación de modelación y migración

--- src/database/migrations/20231015161947-create-categorie.js
+++ src/database/migrations/20231015161947-create-categorie.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Categoris', {
+    await queryInterface.createTable('Categories', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -23,6 +23,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Categoris');
+    await queryInterface.dropTable('Categories');
   }
 };
